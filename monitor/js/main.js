06ee4f3dc80f018a42ef9ca423af9851aad08d17
Uriel-Cruz-Vite <167963030+Uriel-Cruz-Vite@users.noreply.github.com>
2024-05-15 19:06:03 -0600

    Update1

---
+++ monitor/js/main.js
@@ -0,0 +1,37 @@
+let respuesta = document.getElementById("respuesta");
+
+function callApiRequest() {
+  // Hacer una petición para un usuario con ID especifico
+  // .get("https://18.132.200.102/iot-car-control/back-end/apis/getRegistro.php")
+  axios
+    .get("https://18.169.105.156/iot-car-control/back-end/apis/getRegistro.php")
+    .then(function (response) {
+      // manejar respuesta exitosa
+      console.log(response);
+      switch (response.data){
+        case "f":
+          respuestaServidor = "ADELANTE"; break;
+        case "b":
+          respuestaServidor = "ATRÁS"; break;
+        case "l":
+          respuestaServidor = "IZQUIERDA"; break;
+        case "r":
+          respuestaServidor = "DERECHA"; break;
+        default:
+          respuestaServidor = "DETENER"; break;
+      }
+    
+    respuesta.innerHTML="Respuesta: <strong>" + respuestaServidor + "</strong>";
+    
+    })
+    .catch(function (error) {
+      // manejar error
+      console.log(error);
+      console.log("NO");
+    })
+    .finally(function () {
+      // siempre sera executado
+    });
+}
+
+setInterval(callApiRequest, 2000);
